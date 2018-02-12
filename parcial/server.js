a82a6dca9723acd96535a58dd5c8630b3b80de0f
suriel1350 <kato_1350@hotmail.com>
2018-02-12 14:53:29 -0600

    parcial

---
+++ parcial/server.js
@@ -0,0 +1,25 @@
+function route(app, regex, prefix) {
+ app.get(regex, function (req, res) {
+ var type = req.params[0];
+ var path = req.params[1];
+ var file = prefix + type + '/' + path
+ res.sendfile(file);
+ });
+}
+
+var express=require('express');
+var PORT=8080;
+
+var app=express();
+app.get('/', function(req, res){
+	res.sendfile('loginFacebook.html');
+});
+
+app.get('/carrito', function(req, res) {
+ res.sendfile('shoppingcar.html');
+});
+
+route(app, /^\/(assets|dist|img)\/(.*)/, './');
+
+app.listen(PORT);
+console.log('Running on port ' + PORT);
